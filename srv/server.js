83a7656cc771b13471db6522b70b9df91a3b0d00
elgordito123 <dimitri.ratkovitch@neomatic.io>
2020-11-05 11:01:45 +0100

    log JWT

--- srv/server.js
+++ srv/server.js
@@ -1,24 +1,46 @@
-const xsenv = require("@sap/xsenv")
-const passport = require("passport")
+const xsenv = require("@sap/xsenv");
+const passport = require("passport");
 
-var xsuaaCredentials
+var xsuaaCredentials;
 try {
-    xsenv.loadEnv();
-    const JWTStrategy = require("@sap/xssec").JWTStrategy
-    const services = xsenv.getServices({ xsuaa: { tags: "xsuaa" } })
-    xsuaaCredentials = services.xsuaa
-    const jwtStrategy = new JWTStrategy(xsuaaCredentials)
-    passport.use(jwtStrategy)
+  xsenv.loadEnv();
+  const JWTStrategy = require("@sap/xssec").JWTStrategy;
+  const services = xsenv.getServices({ xsuaa: { tags: "xsuaa" } });
+  xsuaaCredentials = services.xsuaa;
+  const jwtStrategy = new JWTStrategy(xsuaaCredentials);
+  passport.use(jwtStrategy);
 } catch (error) {
-    console.warn(error.message)
+  console.warn(error.message);
 }
 
-cds.on('bootstrap', async (app) => {
-    await app.use(passport.initialize())
-    await app.use(
-        passport.authenticate("JWT", { session: false })
-    )
-})
+// Middleware to read JWT sent by JobScheduler
+function jwtLogger(req, res, next) {
+  console.log("===> Binding: $XSAPPNAME: " + xsuaaCredentials.xsappname);
+  console.log("===> Binding: clientid: " + xsuaaCredentials.clientid);
+  console.log("===> Decoding JWT token sent by clientapp");
+  const authHeader = req.headers.authorization;
+  if (authHeader) {
+    const theJwtToken = authHeader.substring(7);
+    if (theJwtToken) {
+      const jwtBase64Encoded = theJwtToken.split(".")[1];
+      const jwtDecoded = Buffer.from(jwtBase64Encoded, "base64").toString(
+        "ascii"
+      );
+      const jwtJson = JSON.parse(jwtDecoded);
+      console.log("===> JWT: audiences: ");
+      jwtJson.aud.forEach((entry) => console.log(`          -> ${entry}`));
+      console.log("===> JWT: scopes: " + jwtJson.scope);
+      console.log("===> JWT: authorities: " + jwtJson.authorities);
+      console.log("===> JWT: client_id: " + jwtJson.client_id);
+    }
+  }
+  next();
+}
 
+cds.on("bootstrap", async (app) => {
+  app.use(jwtLogger);
+  await app.use(passport.initialize());
+  await app.use(passport.authenticate("JWT", { session: false }));
+});
 
-module.exports = cds.server // > delegate to default server.js
+module.exports = cds.server; // > delegate to default server.js
